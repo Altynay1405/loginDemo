d65f10e2eaec0f40a2fe5d5743be1824c035a427
Altynay1405 <altynay.it@gmail.com>
2019-07-08 03:15:04 -0400

    login page demo with localization in kz

---
+++ app/src/main/java/com/example/logindemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.logindemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+   // private EditText UserName;
+    //private EditText Password;
+    //private Button Login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+      //  UserName = (EditText)findViewById(R.id.userName);
+        //Password = (EditText)findViewById(R.id.password);
+        //Login  = (Button)findViewById(R.id.btnLogin);
+
+
+
+    }
+}
